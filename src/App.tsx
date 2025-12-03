45f034fd7f26119af0bc757ab14d00388c3236d7
Shara Raj <shararaj06@gmail.com>
2025-12-03 11:47:31 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/wellness-yoga" element={<WellnessYoga />} />
