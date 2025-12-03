93641ccbf15526bcbf69903b14b62a4a246a9b86
Shara Raj <shararaj06@gmail.com>
2025-12-03 10:24:29 +0530

    Update vite.config.ts
    Updated the file for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,16 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // 👇 ADD THIS LINE
+  base: "/wellmoon-veda-harmony-public/",
+
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger()].filter(
+    Boolean
+  ),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
